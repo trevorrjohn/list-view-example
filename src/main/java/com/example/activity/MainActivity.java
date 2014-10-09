4054217b9f50f1b17ff13ca4992128a427c7ab4f
Trevor John <trevor@john.tj>
2014-10-08 21:03:50 -0400

    getting ready for lunch

--- src/main/java/com/example/activity/MainActivity.java
+++ src/main/java/com/example/activity/MainActivity.java
@@ -4,12 +4,24 @@ import android.app.Activity;
 import android.os.Bundle;
 import com.example.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends Activity {
+  public static final List<String> westWingers  = new ArrayList<String>() {{
+    add("Leo");
+    add("Josh");
+    add("C.J.");
+    add("Jed");
+    add("Charlie");
+    add("Toby");
+    add("Sam");
+    add("Donna");
+  }};
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
-    setTitle("Deckard");
   }
 }
--- src/test/java/com/example/activity/MainActivityTest.java
+++ src/test/java/com/example/activity/MainActivityTest.java
@@ -1,18 +1,45 @@
 package com.example.activity;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.widget.ListView;
+import com.example.R;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowListView;
 
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.robolectric.Robolectric.shadowOf;
 
 @RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
+  private Activity subject;
 
-  @org.junit.Test
-  public void titleIsCorrect() throws Exception {
-    Activity activity = Robolectric.setupActivity(MainActivity.class);
-    assertTrue(activity.getTitle().toString().equals("Deckard"));
+  @Before
+  public void setUp() throws Exception {
+    subject = Robolectric.setupActivity(MainActivity.class);
+  }
+
+  @Test
+  public void itHasAllThePeopleOfTheWestWing() throws Exception {
+    ListView listView = (ListView) subject.findViewById(R.id.list_view);
+    shadowOf(listView).populateItems();
+
+    assertThat(listView.getChildCount()).isEqualTo(MainActivity.westWingers.size());
+  }
+
+  @Test
+  public void whenIClickOnAPersonIAmTakenToTheirPage() {
+    ListView listView = (ListView) subject.findViewById(R.id.list_view);
+    ShadowListView shadowListView = shadowOf(listView);
+    shadowListView.populateItems();
+
+    shadowListView.performHapticFeedback(0);
+
+    Intent nextStartedActivity = shadowOf(subject).getNextStartedActivity();
+    assertThat(nextStartedActivity.getComponent()).isEqualTo(PeopleActivity.class);
   }
 }
